886d89451284eacece87ee9586c4c523c148dcac
DylanTorres03 <dylantorresatv04@gmail.com>
2023-10-20 02:33:55 -0600

    form practica
    angular form

--- s06-Dylan/src/app/form/form.component.ts
+++ s06-Dylan/src/app/form/form.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { FormsModule, NgForm, FormControl, FormGroupDirective } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
+  standalone: true,
+  imports: [FormsModule, MatInputModule, MatFormFieldModule],
 })
 export class FormComponent {
+  formData = {
+    nombre: '',
+    comentario: ''
+  };
+  onSubmit() {
+    console.log(this.formData)
 
+
+  }
+  onReset() {
+    this.formData = {
+      nombre: '',
+      comentario: ''
+
+    }
+
+  }
 }
