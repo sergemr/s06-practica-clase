d491c581191bc348c90874f6334835e7d4b40baa
BrandonZS <abrandon34zs@gmail.com>
2023-10-19 20:50:15 -0600

    Add forms

--- BrandonZamora/s06-BZamora/src/app/app-routing.module.ts
+++ BrandonZamora/s06-BZamora/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'form', component: FormComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
