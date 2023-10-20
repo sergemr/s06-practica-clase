b5a5fc535c76f436a99a0b4869e92c89958e9051
Sebas1901 <145083146+Sebas1901@users.noreply.github.com>
2023-10-19 21:00:05 -0600

    a

---
+++ SCalderon19/06sebastian/src/app/form/form.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {FormsModule} from  '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css'],
+  standalone: true,
+  imports: [FormsModule,  MatFormFieldModule, MatInputModule],
+})
+
+export class FormComponent {
+  formData= {
+    nombre:'',
+    comentario: '',
+  }
+  onSubmit(){
+ console.log(this.formData);
+
+  }
+
+}
